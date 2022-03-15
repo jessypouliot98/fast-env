5e0a26037b3274804c3d82ea6fe624683d9d7874
Jessy Pouliot <jessy.pouliot@renorun.ca>
2022-03-14 22:41:33 -0400

    save

--- config-modules/100_general.zsh
+++ config-modules/100_general.zsh
@@ -4,5 +4,7 @@ USER_ZSH_MODULE="$HOME/.user.zsh"
 
 alias ll="ls -laf"
 alias reload="source $ZSH_CONFIG"
+alias fastenv:pull="cd $FAST_ENV && git pull"
+alias fastenv:push="cd $FAST_ENV && git add . && git commit -m \"save\" && git push"
 alias edit:config="code $FAST_ENV"
 alias edit:user-config="code $USER_ZSH_MODULE"
