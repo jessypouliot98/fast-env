cf533b1a2ac0b2fb51eecc6013e4c06f968282f6
Jessy Pouliot <jessy.pouliot@renorun.ca>
2022-03-14 22:36:28 -0400

    change

--- config-modules/100_general.zsh
+++ config-modules/100_general.zsh
@@ -1,6 +1,8 @@
 ZSH_CONFIG=$HOME/.zshrc
 FAST_ENV=$HOME/.fast-env
+USER_ZSH_MODULE="$HOME/.user.zsh"
 
 alias ll="ls -laf"
 alias reload="source $ZSH_CONFIG"
-alias edit-config="code $FAST_ENV"
+alias edit:config="code $FAST_ENV"
+alias edit:user-config="code $USER_ZSH_MODULE"
