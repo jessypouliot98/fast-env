b8c7bc45bac6c57241053658090b1b62460b8a39
Jessy Pouliot <jessy.pouliot@renorun.ca>
2022-03-18 13:30:58 -0400

    update

--- install.sh
+++ install.sh
@@ -4,17 +4,18 @@ FAST_ENV_DIR="$HOME/.fast-env"
 
 cd $HOME
 
-echo "Cloning Fast-Env"
-git clone https://github.com/jessypouliot98/fast-env.git $FAST_ENV_DIR
+if [ -d FAST_ENV_DIR ]
+  then
+    echo "Cloning Fast-Env"
+    git clone https://github.com/jessypouliot98/fast-env.git $FAST_ENV_DIR
+  else
+    echo "Fast-Env is already installed"
+    echo "Updating Fast-Env"
+    cd $FAST_ENV_DIR
+    git pull
+fi
 
 echo "Adding fast-env to your .zshrc"
-SOURCE_FAST_ENV=$(cat <<-END
-\n
-# START Sourcing fast-env config\n
-source $FAST_ENV_DIR/main.zsh\n
-# END\n
-END
-)
-echo $SOURCE_FAST_ENV >> $HOME/.zshrc
+echo "source $FAST_ENV_DIR/main.zsh" >> $HOME/.zshrc
 
 echo "Close terminal and reopen or run \"source $HOME/.zshrc\" to load new configuration"
