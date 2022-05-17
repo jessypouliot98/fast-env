5bee5e42491de8d14586cc8364f2230c3883de1c
Jessy Pouliot <jessy.pouliot@renorun.ca>
2022-05-17 13:45:18 -0400

    save

--- config-modules/050_ios.zsh
+++ config-modules/050_ios.zsh
@@ -1,2 +1,3 @@
 alias ios:clean="echo Cleaning ios && rm -rf ~/Library/Developer/Xcode/DerivedData && cd ios && xcodebuild clean && cd .."
 alias ios:open="xcrun simctl openurl booted"
+alias ios:list="xcrun simctl list devices"
