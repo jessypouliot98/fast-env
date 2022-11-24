e5585fe472c46cab408eba9fa2d6bb41ff855e8d
Jessy Pouliot <jessypouliot98@gmail.com>
2022-11-24 17:54:17 -0500

    save

--- config-modules/050_ios.zsh
+++ config-modules/050_ios.zsh
@@ -1,6 +1,9 @@
-if [ -d "~/Library/Developer/Xcode" ]; then
-  alias ios:clean="echo Cleaning ios && rm -rf ~/Library/Developer/Xcode/DerivedData && cd ios && xcodebuild clean && cd .."
+XCODE_DIR="$HOME/Library/Developer/Xcode"
+RECORD_OUT_DIR="$HOME/Movies/Recordings"
+
+if [ -d $XCODE_DIR ]; then
+  alias ios:clean="echo Cleaning ios && rm -rf $XCODE_DIR/DerivedData && cd ios && xcodebuild clean && cd .."
   alias ios:open="xcrun simctl openurl booted"
   alias ios:list="xcrun simctl list devices"
-  alias ios:record="xcrun simctl io booted recordVideo ~/Pictures/ios_recording.mp4"
+  alias ios:record="xcrun simctl io booted recordVideo $RECORD_OUT_DIR/ios_recording.mp4"
 fi
