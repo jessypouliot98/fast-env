b3c8231161e3cf3489a99e45670e161165bf8733
Jessy Pouliot <jessypouliot98@gmail.com>
2022-11-28 10:59:43 -0500

    save

--- config-modules/051_android.zsh
+++ config-modules/051_android.zsh
@@ -1,4 +1,5 @@
 export ANDROID_HOME=$HOME/Library/Android/sdk
+RECORD_OUT_DIR="$HOME/Movies/Recordings"
 
 if [ -d $ANDROID_HOME ]; then
   export PATH=$PATH:$ANDROID_HOME/emulator
@@ -12,5 +13,5 @@ if [ -d $ANDROID_HOME ]; then
   alias android:open="echo Opening link && adb shell am start -a android.intent.action.VIEW -d"
   alias android:uninstall="echo Uninstalling apk && adb uninstall $1"
   alias android:record="adb shell screenrecord /sdcard/record.mp4"
-  alias android:record:pull="adb pull /sdcard/record.mp4 ~/Pictures/android_recording.mp4 && adb shell rm /sdcard/record.mp4"
+  alias android:record:pull="adb pull /sdcard/record.mp4 $RECORD_OUT_DIR/android_recording.mp4 && adb shell rm /sdcard/record.mp4"
 fi
