253952d1d5ec64a232b206100300d65203c1ccbe
Jessy Pouliot <jessypouliot98@gmail.com>
2023-02-14 18:21:55 -0500

    save

--- config-modules/100_general.zsh
+++ config-modules/100_general.zsh
@@ -7,3 +7,8 @@ alias fastenv:load="cd $FAST_ENV && git pull"
 alias fastenv:save="cd $FAST_ENV && git add . && git commit -m \"save\" && git push"
 alias edit:config="code $FAST_ENV"
 alias edit:zsh="code $ZSH_CONFIG"
+
+kill:port() {
+  local port="$1"
+  kill -9 $(lsof -ti:$port)
+}
