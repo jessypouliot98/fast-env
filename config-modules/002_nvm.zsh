28f4be6d35daaa66dd8c347e9218a45c96fa0c07
Jessy Pouliot <jessypouliot98@gmail.com>
2024-01-26 18:58:44 -0500

    save

--- config-modules/002_nvm.zsh
+++
@@ -1,9 +0,0 @@
-export NVM_DIR=$HOME/.nvm
-
-if [ -s "$NVM_DIR/nvm.sh" ];
-then
-  # Setup
-  \. "$NVM_DIR/nvm.sh"
-
-  export PATH="$HOME/.config/yarn/global/node_modules/.bin:$PATH" # Make Yarn global CLI available
-fi
